7004186e8af6dc0cf2cc708beef6c9ce0fa9f432
Aaron Imbrock <aimbrock@gmail.com>
2019-07-18 21:13:44 -0700

    Dropdown no longer shows duplicate keywords

--- js/app.js
+++ js/app.js
@@ -2,16 +2,20 @@
 
 
 function Horn(horn) {
-  this.name = horn.name;
+  this.name = horn.title;
   this.image_url = horn.image_url;
   this.hobbies = horn.hobbies;
+  this.keyword = horn.keyword;
+  this.horns = horn.horns;
+  this.description = horn.description;
 }
 Horn.allHorns = [];
+Horn.allKeywords = [];
+
 
 Horn.prototype.render = function() {
   $('main').append('<div class="clone"></div>');
   let hornClone = $('div[class="clone"]');
-
   let hornHtml = $('#horn-template').html(); // change in CSS
 
   hornClone.html(hornHtml);
@@ -21,8 +25,17 @@ Horn.prototype.render = function() {
   hornClone.find('p').text(this.hobbies);
   hornClone.removeClass('clone');
   hornClone.attr('class', this.name);
+  hornClone.attr('class', this.keyword);
+  if (! Horn.allKeywords.includes(this.keyword)) {
+    Horn.allKeywords.push(this.keyword);
+    $('select').append(`'<option>${this.keyword}</option>'`);
+  }
+  // Horn.allKeywords.push(this.keyword);
+  // for (let i=0; i<Horn.allKeywords.length; i++) {
+  //   console.log(Horn.allKeywords[i]);
+    
+  // }
 };
-
 
 Horn.readJson = () => {
   $.get('data/page-1.json', 'json')
@@ -40,3 +53,5 @@ Horn.loadHorns = () => {
 };
 
 $(() => Horn.readJson());
+
+console.log(Horn.allKeywords);
